8aad43099c26d0f90aeda65fc5ebd2b32dffba1d
MHany97 <mohamed.a.hany97@gmail.com>
2022-02-13 00:40:02 +0200

    Final

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$('.animated').smoove({ offset: '20%' });
+
+
+onscroll = function() {
+
+    document.getElementById('navig').classList.add('bg-dark');
+    document.getElementById('navig').classList.add('navbar-dark');
+    document.getElementsByClassName('nav-link').style.color = "white";
+}
